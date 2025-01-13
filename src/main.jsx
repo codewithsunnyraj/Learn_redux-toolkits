48390cc7fbde46a5f47756c75aba41241cfbf4fb
Skulldued <skulldued30@gmail.com>
2025-01-13 23:53:22 +0530

    connect redux in reactjs

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import store from './store/Store.jsx'
 import App from './App.jsx'
+import { ApiProvider } from '@reduxjs/toolkit/query/react'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <ApiProvider store={store}>
     <App />
-  </StrictMode>,
+  </ApiProvider>,
 )
